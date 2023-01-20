796c8e9ec6abe64291dccf13dd8ab0fe24c741e5
Zul Fachrie <zulfahrie12@gmail.com>
2023-01-20 08:20:46 +0700

    First Commit Vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeaderSec from '../components/LandingPage/HeaderSec.vue';
+import HeroSec from '../components/LandingPage/HeroSec.vue';
+import ProductSec from '../components/LandingPage/ProductSec.vue';
+</script>
+
+<template>
+  <HeaderSec/>
+  <HeroSec/>
+  <ProductSec/>
+</template>
